fe4c16d0c2dce66e9ed9dd21d4873459e82a2360
DanyGVS97 <danielgarcia97@aragon.unam.mx>
2026-03-03 20:19:05 -0600

    Add files via upload
    Programa con distintas operaciones logicas

---
+++ Operaciones logicas/app.js
@@ -0,0 +1,12 @@
+var compara = 10 < 8 && 10 == 10 ;
+console.log(compara);
+
+var compara = true && true;
+console.log(compara);
+
+var compara = 20 < 2 || 12 == 12 ;
+console.log(compara);
+
+var compara = !true && true;
+console.log(compara);
+
